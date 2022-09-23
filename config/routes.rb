d0de94363b524cc2b797403123fbb6c96f912055
mark stevens <markspstevens@yahoo.co.uk>
2022-09-23 11:09:08 +0200

    ruby tutorial first part complete

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :friends
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # get 'home/index'
+  root 'home#index'
+
+  get 'home/about'
+  get 'about', to: 'home#about'
+end
